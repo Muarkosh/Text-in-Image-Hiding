01d173a2cb49c5d65f7c87caff673fb0095d1358
Makarosh <50702225+Muarkosh@users.noreply.github.com>
2021-07-15 13:54:48 +0300

    Add files via upload

---
+++ Text In Image Hiding/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿using System.Windows.Forms;
+
+namespace Text_In_Image_Hiding
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialogForm = new System.Windows.Forms.OpenFileDialog();
+            this.Load_button = new System.Windows.Forms.Button();
+            this.Save_button = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialogForm
+            // 
+            this.openFileDialogForm.FileName = "openFileDialogForm";
+            // 
+            // Load_button
+            // 
+            this.Load_button.Location = new System.Drawing.Point(608, 46);
+            this.Load_button.Name = "Load_button";
+            this.Load_button.Size = new System.Drawing.Size(115, 32);
+            this.Load_button.TabIndex = 0;
+            this.Load_button.Text = "Load image";
+            this.Load_button.UseVisualStyleBackColor = true;
+            this.Load_button.Click += new System.EventHandler(this.Load_button_Click);
+            // 
+            // Save_button
+            // 
+            this.Save_button.Location = new System.Drawing.Point(608, 352);
+            this.Save_button.Name = "Save_button";
+            this.Save_button.Size = new System.Drawing.Size(115, 32);
+            this.Save_button.TabIndex = 1;
+            this.Save_button.Text = "Save as .bmp";
+            this.Save_button.UseVisualStyleBackColor = true;
+            this.Save_button.Click += new System.EventHandler(this.Save_button_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(42, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Save_button);
+            this.Controls.Add(this.Load_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.OpenFileDialog openFileDialogForm;
+        public System.Windows.Forms.Button Load_button;
+        public System.Windows.Forms.Button Save_button;
+        private PictureBox pictureBox1;
+    }
+}
+
---
+++ Text In Image Hiding/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_In_Image_Hiding
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class OpenFileDialogForm : Form1
+        {
+            public OpenFileDialogForm()
+            {
+                openFileDialogForm = new OpenFileDialog()
+                {
+                    FileName = "Select an image file",
+                    Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|All files (*.*)|*.*",
+                    Title = "Open image file"
+                };
+                Load_button.Click += new EventHandler(base.Load_button_Click);
+                Controls.Add(Load_button);
+            }
+            /*
+            private void Load_button_Click(object sender, EventArgs e)
+            {
+                if (openFileDialogForm.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        var filePath = openFileDialogForm.FileName;
+                        Image image = Image.FromFile(filePath);
+                        object obj = image;
+                        using (Stream str = openFileDialogForm.OpenFile())
+                        {
+                            Point ulCorner = new Point(100, 100);
+                            pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawImagePoint);
+                            Graphics.DrawImage(image, ulCorner);
+                            this.DrawImagePoint;
+                            //Process.Start("notepad.exe", filePath);
+                        }
+                    }
+                    catch (SecurityException ex)
+                    {
+                        MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                        $"Details:\n\n{ex.StackTrace}");
+                    }
+                }
+            }
+            */
+            /*
+            public void DrawImagePoint(object sender, PaintEventArgs e)
+            {
+                // Create image.
+                Image newImage = Image.FromFile("SampImag.jpg");
+
+                // Create Point for upper-left corner of image.
+                Point ulCorner = new Point(100, 100);
+
+                // Draw image to screen.
+                e.Graphics.DrawImage(newImage, ulCorner);
+            }
+            */
+        }
+
+
+        public void Load_button_Click(object sender, EventArgs e)
+        {
+            //Program.OpenFileDialogForm p = new Program.OpenFileDialogForm();
+            if (openFileDialogForm.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var filePath = openFileDialogForm.FileName;
+                    Image image = Image.FromFile(filePath);
+                    object obj = image;
+                    using (Stream str = openFileDialogForm.OpenFile())
+                    {
+                        Point ulCorner = new Point(100, 100);
+                        pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+                        //Graphics.DrawImage(image, ulCorner);
+                        //this.DrawImagePoint;
+                        //Process.Start("notepad.exe", filePath);
+                    }
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+        private void pictureBox1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+        }
+
+        private void Save_button_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp1 = new Bitmap(typeof(Button), "retriever-puppy.jpg");
+            bmp1.Save("retriever-puppy.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+        }
+    }
+}
